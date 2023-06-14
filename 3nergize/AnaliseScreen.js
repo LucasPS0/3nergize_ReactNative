b315d82e048100f1bdfefcae8456f18108533ecb
LCourtneey <112607752+LucasPS0@users.noreply.github.com>
2023-06-14 05:06:39 -0300

    Added the barchat and the analisys items

--- 3nergize/AnaliseScreen.js
+++ 3nergize/AnaliseScreen.js
@@ -1,18 +1,70 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Dimensions } from "react-native";
+import { connect } from "react-redux";
 
-export default function AnaliseScreen() {
+const AnaliseScreen = ({ valorRS }) => {
+  let valorTotal = valorRS;
+
+  let g = valorTotal * 0.40; // Geração
+  let t = valorTotal * 0.03; // Transmissão
+  let d = valorTotal * 0.16; // Distribuição
+  let e = valorTotal * 0.09; // Encargos
+
   return (
     <View style={styles.container}>
-      <Text>Analise Screen</Text>
+      <View style={[styles.block, { backgroundColor: "#FFCCCC" }]}>
+        <Text style={styles.text}>Geração: {g.toFixed(2)}</Text>
+      </View>
+      <View style={[styles.block, { backgroundColor: "#CCFFCC" }]}>
+        <Text style={styles.text}>Transmissão: {t.toFixed(2)}</Text>
+      </View>
+      <View style={[styles.block, { backgroundColor: "#CCCCFF" }]}>
+        <Text style={styles.text}>Distribuição: {d.toFixed(2)}</Text>
+      </View>
+      <View style={[styles.block, { backgroundColor: "#FFFFCC" }]}>
+        <Text style={styles.text}>Encargos: {e.toFixed(2)}</Text>
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
+    justifyContent: "space-between",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    position: "relative",
+    top: 300
+  },
+  block: {
+    width: Dimensions.get("window").width / 2 - 20,
+    height: 80,
+    margin: 10,
+    borderRadius: 8,
+    alignItems: "center",
     justifyContent: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  text: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "#000",
   },
 });
+
+const mapStateToProps = (state) => {
+  return {
+    valorRS: state.variable.valorRS,
+  };
+};
+
+export default connect(mapStateToProps)(AnaliseScreen);
